3ccc96d854e36985bc885330c1c01ff764d7bda4
Andrii <Luckyhidalgo@gmail.com>
2020-02-20 15:29:01 +0200

    About - main section - added line animation - w\o adaptive.

--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -6,6 +6,28 @@
 	* =============================================
 	* CALLBACK :: start
 	* ============================================= */
+	const initAnimationTimeline = () => {
+		/*function aboutAnimatedLine() {
+			const _tlAbout = new TimelineMax({
+				repeat: -1
+			});
+
+			const _line1 = document.querySelector('[about-line1-js]'),
+				_line2 = document.querySelector('[about-line2-js]');
+
+			_tlAbout
+				.fromTo(_line1, 2.5, {height: '0', ease: "bounce"}, {height: '70px', ease: "bounce"})
+				.fromTo(_line2, 2.5, {width: '140px', ease: "bounce"}, {width: '0', ease: "bounce"}, '-=0.5')
+				.fromTo(_line1, 2.5, {height: '70px', ease: "bounce"}, {height: '0', ease: "bounce"})
+				.fromTo(_line2, 2.5, {width: '0', ease: "bounce"}, {width: '140px', ease: "bounce"}, '-=0.5');
+
+			// _tlAbout
+				// .fromTo(_line1, 2.5, {y: '-100%', ease: "bounce"}, {y: '0', ease: "bounce"})
+				// .fromTo(_line2, 2.5, {x: '100%', ease: "bounce"}, {x: '0', ease: "bounce"}, '-=0.5');
+		}
+
+		aboutAnimatedLine();*/
+	};
 
 	/*
 	* CALLBACK :: end
@@ -26,6 +48,7 @@
 		// ==========================================
 
 		// callback
+		initAnimationTimeline();
 		// ==========================================
 	};
 	initNative();
