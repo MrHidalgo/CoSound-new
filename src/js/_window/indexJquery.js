27a4a193d27a6db4bad03d3d921d20334a5300a8
Andrii <Luckyhidalgo@gmail.com>
2020-02-22 17:21:55 +0200

    About - added animation.

---
+++ src/js/_window/indexJquery.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * @description Window on load.
+ */
+$(window).on("load", (ev) => {
+	initHeaderFixed();
+});
+
+
+/**
+ * @description Window on resize.
+ */
+$(window).on("resize", (ev) => {
+
+});
+
+
+/**
+ * @description Window on scroll.
+ */
+$(window).on("scroll", (ev) => {
+	initHeaderFixed();
+});
+
