f25f8b9aee6833ec54a174c9d7be7f5c6cfe9853
Andrii <Luckyhidalgo@gmail.com>
2020-02-22 20:09:22 +0200

    Homepage - created main section - w\o adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -76,6 +76,35 @@ var initSwiper = function initSwiper() {
 			}
 		});
 	}
+
+	new Swiper('.mainSlider', {
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		speed: 1000,
+		autoplay: {
+			delay: 5000
+		},
+		slidesPerView: 1,
+		spaceBetween: 0,
+		navigation: {
+			nextEl: '.main__arrow--next',
+			prevEl: '.main__arrow--prev'
+		},
+		pagination: {
+			el: '.main__indicator',
+			type: 'custom',
+			renderCustom: function renderCustom(swiper, current, total) {
+				function numberAppend(d) {
+					return d < 10 ? '0' + d.toString() : d.toString();
+				}
+
+				return '<span class="' + current + '">' + numberAppend(current) + '</span>';
+			}
+		}
+	});
 };
 
 /**
--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -25,4 +25,33 @@ const initSwiper = () => {
 			}
 		});
 	}
+
+	new Swiper('.mainSlider', {
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		speed: 1000,
+		autoplay: {
+		  delay: 5000,
+		},
+		slidesPerView: 1,
+		spaceBetween: 0,
+		navigation: {
+			nextEl: '.main__arrow--next',
+			prevEl: '.main__arrow--prev',
+		},
+		pagination: {
+			el: '.main__indicator',
+			type: 'custom',
+			renderCustom: function (swiper, current, total) {
+				function numberAppend(d) {
+					return (d < 10) ? '0' + d.toString() : d.toString();
+				}
+
+				return '<span class="' + current + '">' + numberAppend(current) + '</span>';
+			}
+		},
+	});
 };
